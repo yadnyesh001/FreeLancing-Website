1b34704513e7ac221d926252350343a4e21a9912
Yadnyesh001 <yadnyeshbadgujar2004@gmail.com>
2025-01-28 15:07:17 +0530

    auth

--- backend/index.js
+++ backend/index.js
@@ -1,32 +1,16 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
-
 import authRoutes from './routes/auth.route.js';
-import profileRoutes from './routes/profile.route.js';
-import jobRoutes from './routes/job.route.js';
-import bidRoutes from './routes/bid.route.js';
-
-import connectDB from './lib/db.js';
-
-const app = express();
-
-app.use(express.json());
-app.use(cookieParser());  
 
 dotenv.config();
 
-app.use('/api/v1/auth', authRoutes);
-app.use('/api/v1/profile', profileRoutes);
-app.use('/api/v1/jobs', jobRoutes);
-app.use('/api/v1', bidRoutes);
-app.use('/api/v1/contracts', contractRoutes);
-app.use('/api/v1/payments', paymentRoutes);
-app.use('/api/v1/ratings', ratingRoutes);
-
+const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.use('/api/v1/auth', authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-  connectDB();
 });
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,12 +1,11 @@
 import express from 'express';
-import { signup, login, logout, getMe } from '../controllers/auth.controller.js';
-import { protectRoute } from '../middlewares/auth.middleware.js';
+
+import { signup, login, logout } from '../controllers/auth.controller.js'
 
 const router = express.Router();
 
-router.post('/signup', signup);
-router.post('/login', login);
-router.post('/logout', logout);
-router.get('/me', protectRoute, getMe);
+router.get('/signup', signup);
+router.get('/login', login);
+router.get('/logout', logout);
 
 export default router;
